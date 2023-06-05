dbe088bcfdcd5e3534d1210fadbdda6d22ddaa7d
Elshod701 <jorayevelshod511@gmail.com>
2023-06-05 15:16:53 +0500

    1.1

---
+++ script.js
@@ -0,0 +1,135 @@
+1-masala
+let a=10;
+
+if (a > 0) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// 2-masala
+
+// let a = 3;
+// if(a%2==!0){
+//     console.log('Toq son');
+// } else{
+//     console.log('Juft son');
+// }
+
+// 3-masala
+
+// let a = 4;
+
+// if(a%2==0){
+//     console.log('Juft son');
+// } else{
+//     console.log('Toq son');
+// }
+
+// 4-masala
+
+// let a = 10;
+// let b = 4;
+
+// if (a > 0 && b > 0) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// 5-masala
+// let a = 3;
+// let b = 4;
+// let c = 5;
+
+// if (a > b && a > c) {
+//   console.log("A soni katta");
+// } else if (b > c) {
+//   console.log("B soni katta");
+// } else {
+//   console.log("C soni katta");
+// }
+
+// 6-masala
+
+// let a = prompt("First number: ");
+// let b = prompt("Second number: ");
+// let c = prompt("Third number: ");
+
+// if (b<a && b>c){
+//     console.log("True");
+// }else{
+//     console.log('False');
+// }
+
+// 7-masala
+
+// let a = prompt("First number: ");
+// let b = prompt("Second number: ");
+
+// if(a%2!=0 && b%2!=0){
+//     console.log('True');
+// }else{
+//     console.log('False');
+// }
+
+// 8-masala
+
+// let a = prompt("First number: ");
+// let b = prompt("Second number: ");
+// let c = prompt("Third number: ");
+// let d = prompt("Fourth number: ");
+
+// if (a > 0 && b > 0 && c > 0 && d > 0) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// 9-masala
+
+// let a = prompt("First number: ");
+// let b = prompt("Second number: ");
+// let c = prompt("Second number: ");
+
+// if (a > 0 && b > 0) {
+//   console.log("True");
+// } else if (a > 0 && c > 0) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// 10-masala
+
+// let WeekDay = +prompt("Enter the weekday number");
+
+// if (WeekDay == 1) {
+//   console.log("Monday");
+// } else if (WeekDay == 2) {
+//   console.log("Tuesday");
+// } else if (WeekDay == 3) {
+//   console.log("Wednesday");
+// } else if (WeekDay == 4) {
+//   console.log("Thursday");
+// } else if (WeekDay == 5) {
+//   console.log("Friday");
+// } else if (WeekDay == 6) {
+//   console.log("Saturday");
+// } else if (WeekDay == 7) {
+//   console.log("Sunday");
+// } else {
+//   console.log("Error!");
+// }
+
+// 12-masala
+
+// let a = 13;
+// if (a > 0) {
+//   console.log(a + 1);
+// } else {
+//   console.log(a - 1);
+// }
+
+    
+
